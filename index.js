c48709ddfe495a94a10544c4f1180f54dc924a3e
MecedLoga <mecedloga@gmail.com>
2023-07-27 11:14:36 +0100

    Variables declaration

---
+++ index.js
@@ -0,0 +1,2 @@
+const messageList = ["Vous etes le roi", "Nou avons appris sur vous", "Le soleil apparaitra", "La lune est belle"];
+const astroSign = ["Lion", "Dragon", "Balaine", "Soleil", "Lune"];
