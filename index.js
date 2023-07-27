b165a05b89e621992c66b5e6d4c20a39e6ea6ced
MecedLoga <mecedloga@gmail.com>
2023-07-27 11:34:08 +0100

    using our functions

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const messageList = ["Vous etes le roi", "Nou avons appris sur vous", "Le soleil apparaitra", "La lune est belle"];
 const astroSign = ["Lion", "Dragon", "Balaine", "Soleil", "Lune"];
+
+function getRandomValue(list) {
+    const randomIndex = Math.floor(Math.random() * list.length);
+    return list[randomIndex];
+  }
+ 
+  function generateRandomMessage() {
+    const astrology = getRandomValue(astroSign);
+    const message = getRandomValue(messageList);
+
+    return `Votre horoscope pour aujourd'hui : ${astrology}. Citation inspirante : "${message}`;
+}
+
+// Génère un message aléatoire
+console.log(generateRandomMessage());
